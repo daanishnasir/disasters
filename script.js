b32844bcf2c7769f57b8b5ce79459b98318ed1eb
jdungan <johnadungan@gmail.com>
2013-11-18 23:48:58 -0600

    completed disaster fill

--- script.js
+++ script.js
@@ -1,41 +1,88 @@
-// Create and load map
-$('#map').mapbox('examples.map-zr0njcqy', function(map, tilejson) {
 
-    map.setZoomRange(0, 15);
+jQuery(document).ready(function () {
 
-    // Add share control
-    mapbox.share().map(map).add();
+    // Use Leaflet to implement a D3 geometric transformation.
+    function projectPoint(x, y) {
+        var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+        this.stream.point(point.x, point.y);
+    };
 
-    // Set title and description from tilejson
-    document.title = tilejson.name;
-    $('h1.map-title').text(tilejson.name);
-    $('p.description').text(tilejson.description);
+    var map =  L.mapbox.map('map', 'jdungan.g8c274d0').setView([39, -96], 5),
+        svg = d3.select(map.getPanes().overlayPane).append("svg"),
+        g = svg.append("g").attr("class", "leaflet-zoom-hide");
 
+    d3.json("counties.json", function(collection) {
+        // Reposition the SVG to cover the features.
+        function reset() {
+            var topLeft = bounds[0],
+                bottomRight = bounds[1];
+            svg .attr("width", bottomRight[0] - topLeft[0])
+                .attr("height", bottomRight[1] - topLeft[1])
+                .style("left", topLeft[0] + "px")
+                .style("top", topLeft[1] + "px");                
+            g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
 
-    var container = $('#markerfilters');
-    $.each(tilejson.markers.markers(), function(index, m) {
-        var s = m.data.properties['marker-symbol'];
+            feature.attr("d", path);
+        };
 
-        if (container.find('[href="#' + s + '"]').length) return;
+        var transform = d3.geo.transform({point: projectPoint}),
+            path = d3.geo.path().projection(transform),
+            bounds = path.bounds(collection);
 
-        var el = $(document.createElement('a'))
-            .addClass('markerfilter')
-            .attr('href', '#' + s)
-            .css('background-image', 'url(http://a.tiles.mapbox.com/v3/marker/pin-l-'+s+'+000000.png)')
-            .bind('click', filter);
-        container.append(el);
+        var feature = g.selectAll("path")
+            .data(collection.features)
+            .enter()
+            .append("path")
+            .attr('id',function(d){return 'F'+d.properties.FIPS;})
+            .attr('class','county');
+
+        map.on("viewreset", reset);
+    
+        reset();
     });
+      // Load declarations
+      // ST_FIPS,CO_FIPS,DATE,TYPE,DESC
+     var disaster_count=0;
 
+     // draw the timeline control
+     
+     // d3.select('div#timeline').append().append("svg")
+     //     .attr("width", 200)
+     //     .attr("height", 200).append("circle")
+     //     .attr("cx", 30)
+     //     .attr("cy", 30)
+     //     .attr("r", 20);
+     
+     // loop through the data and display
+     function play(error,declarations){
 
-    $('[href="#all"]').bind('click', filter);
+         var disaster_array=declarations,
+             intervalID;
+         
+         function addDisaster(){
+             this.index = this.index || 0
+        
+             // console.log(this.index);
+             this_dec = disaster_array[this.index]
+             
+             if (this_dec){
+                 var fips='#F'+this_dec.ST_FIPS+this_dec.CO_FIPS
+                 if (fips.length>=5){
+                     d3.select(fips).attr('class','county '+this_dec.TYPE)
+                 }  
+                 this.index+=1               
+             } else {
+                 window.clearInterval(intervalID)
+             }
+         };
+                  
+         intervalID=window.setInterval(addDisaster, 1);
+ 
+     };
+      
+     queue()
+          .defer(d3.csv,'./declarations.csv')
+          .await(play);
+    
+});// end document ready
 
-
-    function filter(e) {
-        container.find('a').removeClass('selected');
-        var id = $(this).addClass('selected').attr('href').replace('#', '');
-        tilejson.markers.filter(function(feature) {
-            return feature.properties['marker-symbol'] == id || id == 'all';
-        });
-        return false;
-    }
-});
