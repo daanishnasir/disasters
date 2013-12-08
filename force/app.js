9f8383cadbae4d8dfdf3a9c47260aac395d03679
jdungan <johnadungan@gmail.com>
2013-12-08 17:28:58 -0600

    animate progression of years

--- force/app.js
+++ force/app.js
@@ -31,114 +31,138 @@ var force = d3.layout.force();
 //  FIPS,ST_FIPS,CO_FIPS,DATE,TYPE,DESC
 d3.json('./disasters.array',function(e,disaster_array){
     
-    disasters = disaster_array//.slice(1000,1200);
-    
-    
-    function charge(d) {
-        return -d.forceR*10
-        // return -Date.parse(Date(d.DCLN_INCIDENT_BGN_DT))/1000000000;
-    };
     // inspired by http://vallandingham.me/building_a_bubble_cloud.html    
 
-    collisionPadding=5;
-    collide = function(jitter){
-        // # return a function that modifies
-        // # the x and y of a node
-        return function(d){
-            nodes.forEach(function(d2){
-              // # check that we aren't comparing a node
-              // # with itself
-              if (d != d2){
-                  // # use distance formula to find distance
-                  // # between two nodes
-                  x = d.x - d2.x
-                  y = d.y - d2.y
-                  distance = Math.sqrt(x * x + y * y)
-                  // # find current minimum space between two nodes
-                  // # using the forceR that was set to match the 
-                  // # visible radius of the nodes
-                  minDistance = d.forceR + d2.forceR + collisionPadding;
-
-                  // # if the current distance is less then the minimum
-                  // # allowed then we need to push both nodes away from one another
-                  if (distance < minDistance){              
-                    // # scale the distance based on the jitter variable
-                    how_close = distance-minDistance
-
-                    distance = (distance - minDistance) / distance * jitter;
-
-                    // # move our two nodes
-                    moveX = x * distance;
-                    moveY = y * distance;
-                    
-                    // move the larger circle
-                    if (d.forceR >= d2.forceR) {
-                        d.x -= moveX;
-                        d.y -= moveY;
-                    } else{
-                        d2.x += moveX;
-                        d2.y += moveY;
-                    }
-                }
-                }
-              })   
+    // collisionPadding=5;
+    // 
+    // collide = function(jitter){
+    //     // # return a function that modifies
+    //     // # the x and y of a node
+    //     return function(d){
+    //         nodes.forEach(function(d2){
+    //           // # check that we aren't comparing a node
+    //           // # with itself
+    //           if (d != d2){
+    //               // # use distance formula to find distance
+    //               // # between two nodes
+    //               x = d.x - d2.x
+    //               y = d.y - d2.y
+    //               distance = Math.sqrt(x * x + y * y)
+    //               // # find current minimum space between two nodes
+    //               // # using the forceR that was set to match the 
+    //               // # visible radius of the nodes
+    //               minDistance = d.forceR + d2.forceR + collisionPadding;
+    // 
+    //               // # if the current distance is less then the minimum
+    //               // # allowed then we need to push both nodes away from one another
+    //               if (distance < minDistance){              
+    //                 // # scale the distance based on the jitter variable
+    //                 inside_distance = distance-minDistance
+    // 
+    //                 move_distance = (inside_distance*2) / distance // * jitter; 
+    //                 moveX = x * move_distance;
+    //                 moveY = y * move_distance;
+    //                 
+    //                 // move the larger node
+    //                 if (d.forceR >= d2.forceR) {
+    //                     d.x += moveX;
+    //                     d.y += moveY;
+    //                 } else{
+    //                     d2.x += moveX;
+    //                     d2.y += moveY;
+    //                 }
+    //               }
+    //             }
+    //           })   
+    //     };
+    // };
+    
+    // collision function from http://mbostock.github.io/d3/talk/20111018/collision.html
+    function collide(node) {
+      var r = node.radius + 16,
+          nx1 = node.x - r,
+          nx2 = node.x + r,
+          ny1 = node.y - r,
+          ny2 = node.y + r;
+      return function(quad, x1, y1, x2, y2) {
+        if (quad.point && (quad.point !== node)) {
+          var x = node.x - quad.point.x,
+              y = node.y - quad.point.y,
+              l = Math.sqrt(x * x + y * y),
+              r = node.radius + quad.point.radius;
+          if (l < r) {
+            l = (l - r) / l * .5;
+            node.x -= x *= l;
+            node.y -= y *= l;
+            quad.point.x += x;
+            quad.point.y += y;
+          }
         }
-        
-        
-    }
-
+        return x1 > nx2
+            || x2 < nx1
+            || y1 > ny2
+            || y2 < ny1;
+      };
+    };
+      
     force.on("tick", function(e) {
-    // //       // dampenedAlpha = e.alpha * 0.1    
-    // //       // # Most of the work is done by the gravity and collide
-    // //       // # functions.
+        var q = d3.geom.quadtree(nodes),
+            i = 0,
+            n = nodes.length;
 
+        while (++i < n) {
+          q.visit(collide(nodes[i]));
+        }
     // move it 
         node
-            .attr("cx", function(d) { return d.y; })
-            .attr("cy", function(d) { return d.x; });
-
+            .attr("cx", function(d) { return d.x; })
+            .attr("cy", function(d) { return d.y; });
     // check for collisions  
-          
-        node.each(collide(1))
+        // node.each(collide(.3))
     });
 
     
     var tooltip = d3.select("body")
-    	.append("div")
+        .append("div")
         .attr('id','tooltip')
-    	.style("position", "absolute")
-    	.style("z-index", "10")
-    	.style("visibility", "hidden");
+        .style("position", "absolute")
+        .style("z-index", "10")
+        .style("visibility", "hidden");
         
     tooltip.content = function (d){
         this.html('')
             // .append('p').text(d.year)
             // .append('p').text(d.disasters.length)
-        
             .append('p').text(d.DCLN_INCIDENT_BGN_DT)
             .append('p').text(d.DSTR_SMRY)    
-    }
+    };
 
     nodes = _.map(disaster_array,function(disaster_year){
         return disaster_year.disasters;
     });
-    
-    nodes = _.flatten(nodes)//.slice(500,1000);
 
+    nodes = _.flatten(nodes).slice(1500,2500);
 
     force
         .nodes(nodes)
-        .gravity(1.5)
+        .gravity(.5)
         .size([h,w])
         .charge(charge)
+
+    function charge(d) {
+    
+        return -d.forceR*10
+        // return -Date.parse(Date(d.DCLN_INCIDENT_BGN_DT))/1000000000;
+    };
+
 
     var node = svg.selectAll("circle")
         .data(nodes)
         .enter()
             .append("circle")
             .attr("class", function(d){return d.style})
-            .attr('cx',w/2)
-            .attr('cy',h/2)
+            // .attr('cx',w/2)
+            // .attr('cy',h/2)
             .attr("r", function(d){
                 var area_count = d.AREAS.length;
                 
--- script.js
+++ script.js
@@ -1,193 +1,172 @@
 
 jQuery(document).ready(function () {
 
-
-    var map =  L.mapbox.map('map', 'jdungan.g8c274d0',{ zoomControl: false }).setView([39, -96], 5),
+    var map = L.mapbox.map('map', 'jdungan.g8c274d0', {zoomControl: false}).setView([39, -96], 5),
         svg = d3.select(map.getPanes().overlayPane).append("svg"),
         g = svg.append("g").attr("class", "leaflet-zoom-hide"),
-        legend,counties;
-        
-        
-        
-    
-    new L.Control.Zoom({ position: 'topright' }).addTo(map);
+        legend, counties, legend_element;
 
-    d3.json("counties.json", function(collection) {
-    //     // Reposition the SVG to cover the features.
-        function reset() {
-            var topLeft = bounds[0],
-                bottomRight = bounds[1];
-                
-            svg .attr("width", bottomRight[0] - topLeft[0])
-                .attr("height", bottomRight[1] - topLeft[1])
-                .style("left", topLeft[0] + "px")
-                .style("top", topLeft[1] + "px");                
-            g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
-            counties.attr("d", path);
-        };
-    
-        // Use Leaflet to implement a D3 geometric transformation.
-        function projectPoint(x, y) {
-            var point = map.latLngToLayerPoint(new L.LatLng(y, x));
-            this.stream.point(point.x, point.y);
-        };
-    
-        var transform = d3.geo.transform({point: projectPoint}),
-            path = d3.geo.path().projection(transform),
-            bounds = path.bounds(collection);
+    function draw_counties(callback) {
+        d3.json("counties.json", function(collection) {
 
-        counties = g.selectAll("path")
-            .data(collection.features,function(d,i) { 
-                return d.properties.FIPS; 
-            })
+            // 250-718-7290
+            // Reposition the SVG to cover the features.
+            function reset() {
+                var topLeft = bounds[0],
+                    bottomRight = bounds[1];
+
+                svg.attr("width", bottomRight[0] - topLeft[0])
+                    .attr("height", bottomRight[1] - topLeft[1])
+                    .style("left", topLeft[0] + "px")
+                    .style("top", topLeft[1] + "px");
+                g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
+                counties.attr("d", path);
+            };
+
+            // Use Leaflet to implement a D3 geometric transformation.
+            function projectPoint(x, y) {
+                var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+                this.stream.point(point.x, point.y);
+            };
+
+            var transform = d3.geo.transform({
+                point: projectPoint
+            }),
+                path = d3.geo.path().projection(transform),
+                bounds = path.bounds(collection);
+
+            counties = g.selectAll("path")
+                .data(collection.features, function(d, i) {
+                    return d.properties.FIPS;
+                })
                 .enter()
-                    .append("path")
-                    .attr('class', 'county')
-                    .attr('id', function(d) {
-                        return 'F' + d.properties.FIPS;
-                    });
-        map.on("viewreset", reset);
-        reset();
+                .append("path")
+                .attr('class', 'county')
+                .attr('id', function(d) {
+                    return 'F' + d.properties.FIPS;
+                });
+            map.on("viewreset", reset);
+            reset();
+            callback()
+        
+        });
+    }
+
+    function display_disasters(error,counties,years){
+
+        new L.Control.Zoom({ position: 'topright' }).addTo(map);
+
+        var LegendControl = L.Control.extend({
+            options: {
+                position: 'topleft'
+            },
+            onAdd: function (map) {
+                // create the control container with a particular class name
+                legend_element= L.DomUtil.create('div', 'headlines');
+                //identify in the global space
+                legend = d3.select(legend_element).attr('class','year');
+                return legend_element;
+            },
         });
 
+        map.addControl(new LegendControl());
 
-
-    
-    var LegendControl = L.Control.extend({
-        options: {
-            position: 'topleft'
-        },
-
-        onAdd: function (map) {
-            // create the control container with a particular class name
-            legend_element= L.DomUtil.create('div', 'headlines');
-            //identify in the global space
-            legend = d3.select(legend_element).attr('class','year');
-            return legend_element;
-        },
+        legend.addKey = function (k){        
+            this.key_data= this.key_data ||['',''];
+            this.key_data.shift()
+            this.key_data.push(k)
         
-
-
-
-    });
-
-
-    map.addControl(new LegendControl());
-    
-    legend.addKey = function (k){        
-        this.key_data= this.key_data ||['',''];
-        this.key_data.shift()
-        this.key_data.push(k)
+            key=this.selectAll('text').data(this.key_data);
         
-        key=this.selectAll('text').data(this.key_data);
+            // enter
+            key.enter()
+                .append('text')
+                .attr('class','year')
+                // .text(String);
         
-        // enter
-        key.enter()
-            .append('text')
-            .attr('class','year')
-            // .text(String);
+            // Update
+            key.text(String)
         
-        // Update
-        key.text(String)            
-        
-    };
-             
-            var force = d3.layout.force(),
-            nodes=[{a:5},{a:4},{a:3}];
-            function charge(d) {
-                    
-                  return d.a * -5600;
-            };
-            
-            
-            
-            var node = g.selectAll("circle")
-                .data(nodes)
-                .enter()
-                    .append("circle")
-                        .attr("class", "node")
-                        .attr("r", function(d){return d.a*10})
-                                    
-            force
-                .size ([1000,1000])
-                .nodes(nodes)
-                .charge(charge)
-                .gravity(10)
-                .on("tick", function(e) {
-                    node
-                        .attr("cx", function(d) { return d.x; })
-                        .attr("cy", function(d) { return d.y; });
-                })
-                .start()
-                
-                
-                
-    
-    // legend.addKey([' of',' prowess',' bold']);
-    
-     // loop through the data and display
+        };
+
+         var minYear = _.min(years,function(item){return item.year;});    
+         var maxYear = _.max(years,function(item){return item.year;});    
+
+         var margin = {top: 50, right: 100, bottom: 20, left: 30},
+             width = 1000 - margin.left - margin.right,
+             height = 500 - margin.top - margin.bottom;
+
+         var x = d3.time.scale()
+          .domain([new Date(minYear.year), new Date(maxYear.year)])
+          .nice(d3.time.year)
+          .rangeRound([0,width]);
+
+         var scale_svg = d3.select(legend_element).append("svg")
+             .attr("width", width + margin.left + margin.right)
+             .attr("height", height + margin.top + margin.bottom)
+             .append("g")
+                 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+         scale_svg.append('circle')
+             .attr('r',30)
+             .attr('cy',10)
+             .attr('class','date_viewer')
+             .attr('cx',function(d){return x(new Date(minYear.year));});
      
-     function play(error,declarations){
-
-         var disaster_array=declarations,
-             current_description,
-             county_paths=[],
-             affected_counties=[],
-             intervalID;
-         
-         function displayDisaster(){
-            // d3.select(fips).attr('class','county '+this_dec.TYPE)
-             if (affected_counties.length>0){
+         var scale = scale_svg.append("g")
+          .attr("class", "x axis")
+          .call(d3.svg.axis().scale(x).orient("bottom"));
+           
+         var date_viewer= d3.select('.date_viewer')
+         //set the intial (do nothing) transistion
+         var date_move = date_viewer.transition()
+          
+         _.each(years, function(year){
+             //  add a new transition to the chain
+             date_move = date_move.transition()
+                 .duration(3000)
+                 .attr('cx',function(d){return x(new Date(year.year));})
+                 .each('start',function(){
+                     disasters=year.disasters;
+                     _.each(disasters,function(disaster){
+                         _.each(disaster.counties,function(county){
                  
-                legend.addKey(affected_counties[0].DATE)
-                legend.addKey(affected_counties[0].DESC)
-                 // d3.select(county_paths).clased(d.TYPE);
-             }
-             affected_counties=[];
-             county_paths=[];
-         };
-         
-         
-         
-         function newDeclaration(){
-             this.index = this.index || 0;
-             this_declaration = disaster_array[this.index];
-                          
-             if (this_declaration){
-                 
-                 
-                 if (this_declaration.CO_FIPS){
-                                          
-                     current_description = current_description || this_declaration.DESC;
-                 
-                     if(this_declaration.DESC!=current_description){
-                         displayDisaster();
-                         current_description=this_declaration.DESC;
-                     }
-                 
-                     affected_counties.push(this_declaration)
+                             if (county.fips_co){
+                                 
+                                 disaster_paths = d3.select('#F' + county.fips_co)
+                                 
+                             } else {
+                                 disaster_paths = d3.selectAll('[id^=F' + county.fips_st +']')   
+                             }
                      
-                     d3.select('#F'+this_declaration.FIPS).attr('class','county '+this_declaration.TYPE)
-                                          
-                 }
-                 
-                 this.index+=1                                
+                                 // legend.addKey(affected_counties[0].DATE)
+                                 // legend.addKey(affected_counties[0].DESC)
+                             
+                             disaster_paths    
+                                .transition()
+                                .duration(1450)
+                                .attr('class','county '+disaster.style)
+                                .style('fill-opacity', 0.60)
+                                .transition()
+                                .duration(1450)
+                                .style('fill-opacity', 0)
+                                .transition()
+                                .attr('class', 'county')
 
-             } else {
-                 window.clearInterval(intervalID)
-             }
-         };
-                  
-         intervalID=window.setInterval(newDeclaration, 10);
- 
-     };//end play
-      
-     // Load declarations
-     // ST_FIPS,CO_FIPS,DATE,TYPE,DESC
+                         })//end county loop
 
-     queue()
-          .defer(d3.csv,'./declarations.csv')
-          .await(play);
+                     });// end disaster lopp
+                     
+                 })// end each function
+             
+         });// end year loop
+
+     };//end display_disasters
+     
+    queue()
+        .defer(draw_counties)
+        .defer(d3.json,'./disasters.array')
+        .await(display_disasters);
     
 });// end document ready
 
